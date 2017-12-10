d90d29a1a3056c0afe94e5357723a05f4919284f
levandiep44@gmail.com <levandiep44@gmail.com>
2017-12-10 09:20:56 +0700

    Update SERVER.js

--- SERVER.js
+++ SERVER.js
@@ -7,7 +7,7 @@ app.set("views", "./views");
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
-server.listen(process.env.PORT,3000");
+server.listen(process.env.PORT,3000);
 
 var mangUser =[];
 //khoi tao io lang nghe
